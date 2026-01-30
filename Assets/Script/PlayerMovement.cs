134c24f5483851f0a67464632fc1c02e39b491ea
priyanshukala171-cyber <priyanshukala171@gmail.com>
2026-01-31 00:11:55 +0530

    update player movement

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+    Rigidbody rb;
+    Vector3 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveZ = Input.GetAxisRaw("Horizontal"); // A, D
+        float moveX = Input.GetAxisRaw("Vertical");   // W, S
+
+        moveInput = new Vector3(moveZ, 0f, moveX).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+}
